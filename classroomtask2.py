d230bc15aa66362b355ce1f4b58ee85a7337a402
gauravdal <gaurav.bamania@dal.ca>
2019-02-04 15:28:58 -0400

    binary format of ipaddress and mask

--- classroomtask2.py
+++ classroomtask2.py
@@ -1,4 +1,16 @@
 class ipaddress(object):
-	def __init__ (self, ipaddr, cidr):
-		ipaddr_val = []
-		self.ipaddr =  ipaddr.split(".")
+	def __init__ (self, ipaddr, mask):
+		self.ipaddr_list =  ipaddr.split(".")
+		self.mask_list =  mask.split(".")
+
+	def print_addr(self):
+		for item in self.ipaddr_list:
+			print(item,bin(int(item)),sep="\t", end =".")
+
+		for item in self.mask_list:
+			print(item,bin(int(item)), sep ="\t",end = ".")
+
+
+ipaddress1 = ipaddress("192.168.1.0","255.255.255.0")
+
+ipaddress1.print_addr()
